ffaf82db84f2ed8fa7494d9eaf90e1e43751eaa9
wswalker87 <scott@wscottwalker.com>
2025-12-06 10:11:55 -0600

    initial commit

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-console.log("HELLO PAPA PLATOON!")
+console.log("HELLO INDIA PLATOON!")
 // Your function(s) should go here that will interact with the webpage or DOM
+
+// Create the function to choose a target number unknow to the user
+function getTargetNumber(min, max) {
+    let minCeiling = Math.ceil(min);
+    let maxFloored = Math.floor(max)
+    
+}
+
+let targetNumber = Math.floor(Math.random() * 10)
+console.log(targetNumber)
+
+
+// Take the users input
+
+// Compare the users input against the target number
+
+// if the guess is correct, say so, if not return a direction to guess next
+
+// If the guess was wrong, insert the guess into the previous guess list as a list item
